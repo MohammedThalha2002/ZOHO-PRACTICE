c2a5f1d43af1a7b9152b5d67fbfc7089c6f5f1fa
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2022-12-10 16:08:40 +0530

    commit

---
+++ recursion/BinarySearch.java
@@ -0,0 +1,43 @@
+package recursion;
+import java.util.Scanner;
+
+// Using Recursion
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        System.out.print("Enter the element need to search : ");
+        int val = sc.nextInt();
+
+        int index = binarySearch(arr, val, 0, arr.length-1);
+
+        if(index == -1)
+            System.out.println("The element is not found");
+        else 
+            System.out.println("The element is found at the index " + index);
+        sc.close();
+
+    }
+
+    static int binarySearch(int[] arr, int val, int low, int high) {
+        // No element found
+        if (low > high)
+            return -1;
+
+        int mid = (high + low) / 2;
+
+        // VALUE FOUND
+        if (val == arr[mid])
+            return mid;
+        // Traverse left
+        else if (val < arr[mid])
+            return binarySearch(arr, val, low, mid - 1);
+        // Traverse right
+        else
+            return binarySearch(arr, val, mid + 1, high);
+    }
+}
+
--- searching/BinarySearch.java
+++ searching/BinarySearch.java
@@ -3,37 +3,35 @@ import java.util.Scanner;
 public class BinarySearch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
         System.out.print("Enter the element need to search : ");
         int val = sc.nextInt();
 
-        int index = binarySearch(arr, val, 0, arr.length-1);
+        int index = binarySearch(arr, val, 0, arr.length - 1);
 
-        if(index == -1)
+        if (index == 0)
             System.out.println("The element is not found");
-        else 
-            System.out.println("The element is found at the index " + index);
+        else
+            System.out.println("The element is found at the position " + (index + 1));
         sc.close();
 
     }
 
     static int binarySearch(int[] arr, int val, int low, int high) {
         // No element found
-        if (low > high)
-            return -1;
+        int mid;
+        while (low <= high) {
+            mid = (high + low) / 2;
 
-        int mid = (high - low) / 2 + low;
-
-        // VALUE FOUND
-        if (val == arr[mid])
-            return mid;
-        // Traverse left
-        else if (val < arr[mid])
-            return binarySearch(arr, val, low, mid - 1);
-        // Traverse right
-        else
-            return binarySearch(arr, val, mid + 1, high);
+            if (val == arr[mid])
+                return mid;
+            else if (val < arr[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return 0;
     }
 }
