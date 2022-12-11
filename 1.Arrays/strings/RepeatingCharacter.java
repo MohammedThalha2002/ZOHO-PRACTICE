09145c2c9893b2a7f65e6f6e5b9c812e5919dac0
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2022-12-11 09:08:16 +0530

    commit

---
+++ 1.Arrays/strings/RepeatingCharacter.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+// 1. Find first repeating character in a String
+
+// Going to solve using HASHMAP in java
+// HASHMAP Basics --> https://www.youtube.com/watch?v=H62Jfv1DJlU
+
+public class RepeatingCharacter {
+    public static void main(String[] args) {
+        HashMap<Character, Integer> charMap = new HashMap<>();
+        String str = "abccde";
+        int val = 1;
+
+        // Add all the characters in the hashMap
+        for (int i = 0; i < str.length(); i++) {
+            charMap.put(str.charAt(i), val + (charMap.get(str.charAt(i)) == null ? 0 : 1));
+        }
+        System.out.println(charMap);
+        // Output of the above codes
+        // {a=1, b=1, c=2, d=1, e=1}
+
+        for (int i = 0; i < str.length(); i++) {
+            if (charMap.get(str.charAt(i)) > 1)
+                System.out.println("The repeating character is : " + str.charAt(i));
+        }
+    }
+
+}
---
+++ Strings/FindFirstRepeatingChar.java
@@ -0,0 +1,10 @@
+// 1) Program to find the first repeating character in a string
+package Strings;
+
+public class FindFirstRepeatingChar {
+    public static void main(String[] args) {
+        String str = "abccde";
+        String[] hash = new String[str.length()];
+        System.out.println(hash.length);
+    }
+}
