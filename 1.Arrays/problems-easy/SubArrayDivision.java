89010ac6b8b1e45e603c1443b79318a0a87e2f19
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2022-12-17 17:58:06 +0530

    commit

---
+++ 1.Arrays/problems-easy/SubArrayDivision.java
@@ -0,0 +1,19 @@
+public class SubArrayDivision {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 1, 3, 2 };
+        int win_size = 2;
+        int val = 3;
+
+        findValue(arr, win_size, val);
+    }
+
+    static int findValue(int[] arr, int win_size, int val) {
+        int totalNum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i + win_size < arr.length) {
+                
+            }
+        }
+        return totalNum;
+    }
+}
