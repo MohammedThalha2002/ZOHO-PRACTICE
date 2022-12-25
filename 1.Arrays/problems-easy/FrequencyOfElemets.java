34d33ef13714fbfabf3d4c97bda1ef459e0a3b8b
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2022-12-25 19:15:31 +0530

    commit

--- 1.Arrays/problems-easy/FrequencyOfElemets.java
+++ 1.Arrays/problems-easy/FrequencyOfElemets.java
@@ -5,7 +5,7 @@ public class FrequencyOfElemets {
     public static void main(String[] args) {
         int[] arr = { 2, 2, 3, 4, 2, 1, 1, 4, 4, 4, 6, 3 };
 
-        System.out.println("Before sorting : " + Arrays.toString(arr));
+        System.out.println("Elements : " + Arrays.toString(arr));
 
         // Mapping in hashmap
         HashMap<Integer, Integer> hashMap = new HashMap<>();
@@ -16,18 +16,5 @@ public class FrequencyOfElemets {
         // Print the frequency of the elements
         System.out.println(hashMap);
 
-    }
-
-    static void sortArray(int arr[]) {
-        for (int i = 1; i < arr.length; i++) {
-            int val = arr[i];
-            int index = i;
-
-            while (index > 0 && arr[index - 1] > val) {
-                arr[index] = arr[index - 1];
-                index--;
-            }
-            arr[index] = val;
-        }
     }
 }
